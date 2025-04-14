7db78240e21b8f829faaa061270bf0784865ff71
sauravautomation <sauravqa@gmail.com>
2025-04-14 22:14:27 +0530

    Initial commit

---
+++ src/main/java/com/saurav/learn_ci_cd/App.java
@@ -0,0 +1,13 @@
+package com.saurav.learn_ci_cd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/saurav/learn_ci_cd/AppTest.java
@@ -0,0 +1,20 @@
+package com.saurav.learn_ci_cd;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	public void test1() {
+		Assert.assertTrue("saurav".equalsIgnoreCase("saurav"));
+	}
+	
+	
+	
+	
+	
+}
