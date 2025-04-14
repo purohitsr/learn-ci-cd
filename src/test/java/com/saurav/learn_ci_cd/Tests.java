4762613657313e9de74dc4189aa95048b0a2f7d2
sauravautomation <sauravqa@gmail.com>
2025-04-14 22:35:08 +0530

    modification2

--- src/test/java/com/saurav/learn_ci_cd/Tests.java
+++ src/test/java/com/saurav/learn_ci_cd/Tests.java
@@ -6,11 +6,16 @@ import org.testng.annotations.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+public class Tests {
 	
 	@Test
 	public void test1() {
 		Assert.assertTrue("PlayWrite".equalsIgnoreCase("PlayWrite"));
+	}
+	
+	@Test
+	public void test2() {
+		Assert.assertTrue("Selenium".equalsIgnoreCase("Selenium"));
 	}
 	
 	
