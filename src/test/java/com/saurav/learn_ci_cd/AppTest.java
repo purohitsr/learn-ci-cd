7b6d3baaca07dd3b8a580e04b41dfc50d48d5f2d
sauravautomation <sauravqa@gmail.com>
2025-04-14 22:21:56 +0530

    modification1

--- src/test/java/com/saurav/learn_ci_cd/AppTest.java
+++ src/test/java/com/saurav/learn_ci_cd/AppTest.java
@@ -10,7 +10,7 @@ public class AppTest {
 	
 	@Test
 	public void test1() {
-		Assert.assertTrue("Selenium".equalsIgnoreCase("Selenium"));
+		Assert.assertTrue("PlayWrite".equalsIgnoreCase("PlayWrite"));
 	}
 	
 	
